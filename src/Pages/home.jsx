1bd7ce7299cace06139a9b8b7062aa6e91779428
micahjj1989 <micahjj1989@gmail.com>
2023-10-14 22:07:20 -0500

    Test preview branch

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Home () {
     return (
         <>
-            <h1>Home</h1>
+            <h1>Home Preview</h1>
             <Link to="/new-york/perform">New York</Link>
         </>
     )
